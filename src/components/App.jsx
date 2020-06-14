ebfa0f2e459ed87a32b3ae529dc39ea37f4cae0f
Maksym 19 <mherasevych@sunscript.com.ua>
2020-06-14 15:49:26 +0300

    Init commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Aside from './Aside';
+import Main from './Main';
+
+function App() {
+  return (
+    <>
+      <Aside />
+      <Main />
+    </>
+  );
+}
+
+export default App;
